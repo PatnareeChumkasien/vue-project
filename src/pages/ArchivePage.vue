8e2f689d8a5bbbe484d5d14db5eaad5e325337e9
Patnaree <ch.patnaree_st@tni.ac.th>
2020-05-27 15:02:46 +0700

    CardList

---
+++ src/pages/ArchivePage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <CardList :item= "archives"></CardList>
+    </div>
+</template>
+
+<script>
+import CardList from '../components/CardList.vue'
+export default {
+    name: 'ArchivePage',
+    components: { CardList },
+    data(){
+        return {
+            archives:[
+                {
+                    date: "25-05-2020",
+                    time: "17:00",
+                    tasks: [ { id: 1, title: "Item A", status: "done" },
+                        { id: 2, title: "Item B", status: "done" }
+                    ]
+                },
+                {
+                    date: "26-05-2020",
+                    time: "17:00",
+                    tasks: [
+                        { id: 3, title: "Item C", status: "done" },
+                        { id: 4, title: "Item D", status: "done" }
+                    ]
+                }
+            ]
+        }
+    }
+}
+</script>
